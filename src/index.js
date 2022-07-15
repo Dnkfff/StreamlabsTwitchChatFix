d4d4449289206c430a0b0229e7adc9d040726609
Bob <ddynikov@gmail.com>
2022-07-15 17:18:40 +0300

    refoctoring

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css.CSS';
 import App from './App.jsx';
-import ToDoList from './ToDoList';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
